80252919bf0fdd37833cf94a5618eb5ba7f55af6
Matisalinas23 <maatisalinas18@gmail.com>
2026-06-17 12:35:56 -0300

    refactor: simplify method colors using Tailwind CSS utility classes and extract EndpointDashboard component

--- src/views/dashboard.tsx
+++ src/views/dashboard.tsx
@@ -1,8 +1,43 @@
 import { Aside } from "../components/dashboard/Aside"
 import { useSelectionStore } from "@/store/useSelectionStore"
-import { methodColors } from "@/data/methodColors"
 import { useEffect, useState } from "react"
 import type { IEndpoint } from "@/interfaces/project.interface"
+
+const methodColors = {
+    GET: "bg-green-400",
+    POST: "bg-yellow-300",
+    PUT: "bg-blue-400",
+    DELETE: "bg-red-400",
+    PATCH: "bg-purple-400",
+    HEAD: "bg-lime-400",
+    OPTIONS: "bg-orange-400"
+}
+
+const EndpointDashboard = () => {
+    const { selectedEndpoint } = useSelectionStore()
+    return (
+        <div className="flex-1">
+            <header className="w-full py-4 px-6 border-b border-borders flex justify-between">
+                <div className="w-120 h-12 border border-borders rounded-xl bg-neutral-100">
+                </div>
+
+                <div className="h-12 w-12 rounded-full border-2 border-border-strength">
+                </div>
+            </header>
+
+            <div className="px-8 py-4">
+                <h3 className="text-xl mb-4">{selectedEndpoint?.name}</h3>
+                <div className="flex items-center gap-6">
+                    <div className="flex items-center gap-2">
+                        <div className={` rounded-full w-3 h-3 ${methodColors[selectedEndpoint?.method ?? "GET"]}`}></div>
+                        <h3 className="text-md font-medium">{selectedEndpoint?.method}</h3>
+                    </div>
+                    <div className="h-6 w-full border rounded-md"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default function Dashboard() {
     const { selectedEndpoint } = useSelectionStore()
@@ -13,41 +48,10 @@ export default function Dashboard() {
         setEndpointDashboard(selectedEndpoint)
     }, [selectedEndpoint])
 
-    const getMethodColor = () => {
-        const method = methodColors.find((m) => m.method === endpointDashboard.method)
-
-        if (!method) {
-            console.error("El metodo no existe", method)
-            return "#dddddd"
-        }
-
-        return method.color
-    }
-
-    const methodColor = endpointDashboard ? getMethodColor() : null
-
     return (
-        <div className="flex">
+        <div className="flex text-neutral-600">
             <Aside />
-
-            <div className="flex-1">
-                <header className="w-full py-4 px-6 border-b border-borders flex justify-between">
-                    <div className="w-120 h-12 border border-borders rounded-xl bg-neutral-100">
-                    </div>
-
-                    <div className="h-12 w-12 rounded-full border-2 border-border-strength">
-                    </div>
-                </header>
-
-                <div>
-                    <div className="px-8 my-4 flex items-center gap-2">
-                        <div className="py-1.5 px-3 w-fit rounded-md" style={{ backgroundColor: methodColor }}>
-                            <h3 className="text-[14px] text-white font-medium">{endpointDashboard?.method}</h3>
-                        </div>
-                        <h3 className="text-xl pb-1">{endpointDashboard?.name}</h3>
-                    </div>
-                </div>
-            </div>
+            <EndpointDashboard />
         </div>
     )
 }
