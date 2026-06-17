5595fce34b1c021d099dd07843b2b704ad30b9ec
Matisalinas23 <maatisalinas18@gmail.com>
2026-06-17 14:45:16 -0300

    feat: refactor store to use activeProject/activeModule/activeEndpoint and add projects state

--- src/store/useSelectionStore.ts
+++ src/store/useSelectionStore.ts
@@ -2,34 +2,32 @@ import { create } from 'zustand'
 import type { IProject, IModule, IEndpoint } from '@/interfaces/project.interface'
 
 interface SelectionState {
-  selectedProject: IProject | null
-  selectedModule: IModule | null
-  selectedEndpoint: IEndpoint | null
-  setSelectedProject: (project: IProject | null) => void
-  setSelectedModule: (module: IModule | null) => void
-  setSelectedEndpoint: (endpoint: IEndpoint | null) => void
-  clearSelection: () => void
+  projects: IProject[]
+  activeProject: IProject | null
+  activeModule: IModule | null
+  activeEndpoint: IEndpoint | null
+
+  setProjects: (projects: IProject[]) => void
+  setActiveProject: (project: IProject | null) => void
+  setActiveModule: (module: IModule | null) => void
+  setActiveEndpoint: (endpoint: IEndpoint | null) => void
 }
 
 export const useSelectionStore = create<SelectionState>((set) => ({
-  selectedProject: null,
-  selectedModule: null,
-  selectedEndpoint: null,
-  setSelectedProject: (project) => set({
-    selectedProject: project,
-    selectedModule: null,
-    selectedEndpoint: null
-  }),
-  setSelectedModule: (module) => set({
-    selectedModule: module,
-    selectedEndpoint: null
-  }),
-  setSelectedEndpoint: (endpoint) => set({
-    selectedEndpoint: endpoint
-  }),
-  clearSelection: () => set({
-    selectedProject: null,
-    selectedModule: null,
-    selectedEndpoint: null
-  })
+  projects: [],
+  activeProject: null,
+  activeModule: null,
+  activeEndpoint: null,
+
+  setProjects: (projects) => set({ projects }),
+  setActiveProject: (project) => set((state) => ({
+    activeProject: state.activeProject === project ? null : project
+  })),
+  setActiveModule: (module) => set((state) => ({
+    activeModule: state.activeModule === module ? null : module,
+    
+  })),
+  setActiveEndpoint: (endpoint) => set((state) => ({
+    activeEndpoint: state.activeEndpoint === endpoint ? null : endpoint
+  }))
 }))
