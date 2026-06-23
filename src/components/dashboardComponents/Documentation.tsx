44573c9a40c3699997dd33c0e086a7985e574109
Matisalinas23 <maatisalinas18@gmail.com>
2026-06-23 17:15:05 -0300

    feat: documentation component shows description and notes from the endpoint and can updates description and notes

--- src/components/dashboardComponents/Documentation.tsx
+++ src/components/dashboardComponents/Documentation.tsx
@@ -1,9 +1,12 @@
-export const Documentation = () => {
+export const Documentation = ({description}: {description: string}) => {
     return (
-        <div>
+        <div className="flex flex-col gap-4">
             <h2>DOCUMENTACIÓN</h2>
-            <div className="w-full h-64 border border-borders rounded-lg">
+            <div className="w-full min-h-24 border border-borders rounded-xl p-4 flex flex-col gap-2">
+                <h3 className="font-medium">Descripcion del endpoint:</h3>
+                <p className="text-sm text-neutral-600 mb-8">{description}</p>
 
+                <textarea placeholder="Añadir notas" className="w-full min-h-24 border border-dashed border-neutral-400 px-2 py-1" />
             </div>
         </div>
     )
