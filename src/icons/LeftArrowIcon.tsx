555814ed59fc9f9acd534e860158f004a61f6c08
Matisalinas23 <maatisalinas18@gmail.com>
2026-06-23 16:23:24 -0300

    feat: add LeftArrow and RightArrow icon components

---
+++ src/icons/LeftArrowIcon.tsx
@@ -0,0 +1,5 @@
+export const LeftArrowIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="currentColor">
+        <path d="M560-280 360-480l200-200v400Z"/>
+    </svg>
+)
---
+++ src/icons/RightArrowIcon.tsx
@@ -0,0 +1,5 @@
+export const RightArrowIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="100%" fill="currentColor">
+        <path d="M400-280v-400l200 200-200 200Z"/>
+    </svg>
+)
