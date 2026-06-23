9552f9322d3676410ddbea6abdf5176d4b6ee5ed
Matisalinas23 <maatisalinas18@gmail.com>
2026-06-23 17:15:53 -0300

    refatcor: endpoint data was updated in request body property

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -19,10 +19,10 @@ const projects: IProject[] = [
                         queryParameters: [],
                         pathParameters: [],
                         headers: [],
-                        requestBody: "",
+                        requestBody: "{\n    'name': 'string',\n    'email': 'string'\n    'addresses': [\n        {\n            'street': 'string'\n        }\n    ]\n}",
                         responses: {
                             status: 200,
-                            body: "{\n    'id': 1\n    'name': 'Matias'\n    'email': 'matisalnico@gmail.com'\n    'addresses': [\n        {\n            'id': 1\n            'address': 'Avenida Siempreviva 123'\n        },\n        {\n            'id': 2,\n            'address': 'Avenida Siempreviva 124'\n        },\n        {\n            'id': 3,\n            'address': 'Avenida Siempreviva 125'\n        }\n    ]\n}",
+                            body: "{\n    'id': 1\n    'name': 'Matias'\n    'email': 'matisalnico@gmail.com'\n    'addresses': [\n        {\n            'id': 1\n            'street': 'Avenida Siempreviva 123'\n        },\n        {\n            'id': 2,\n            'address': 'Avenida Siempreviva 124'\n        },\n        {\n            'id': 3,\n            'address': 'Avenida Siempreviva 125'\n        }\n    ]\n}",
                             description: "OK"
                         },
                         note: "",
